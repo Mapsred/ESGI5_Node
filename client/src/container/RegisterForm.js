91217584a5e0911eae1fb2ed15c2cbbb18257290
Jérémy Haudry <jeremy-haudry@hotmail.fr>
2019-02-14 23:13:54 +0100

    validateurs register :oula:

--- client/src/container/RegisterForm.js
+++ client/src/container/RegisterForm.js
@@ -3,6 +3,7 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import FormGroup from '@material-ui/core/FormGroup';
+import FormControl from '@material-ui/core/FormControl';
 import Button from '@material-ui/core/Button';
 import FormLabel from '@material-ui/core/FormLabel';
 
@@ -30,48 +31,60 @@ export default class RegisterForm extends React.Component {
     render() {
         return (
             <Grid container justify="center">
-                <form onSubmit={this.handleSubmit}>
+                <form noValidate onSubmit={this.handleSubmit}>
                     <FormLabel component="legend">Inscription</FormLabel>
                     <FormGroup>
-                        <TextField
-                            id="email"
-                            label="Email"
-                            onKeyUp={this.handleKeyUp('email')}
-                            margin="normal"
-                            type="email"
-                            style={{width: 200}}
-                        />
-                        <TextField
-                            id="firstname"
-                            label="Firstname"
-                            onKeyUp={this.handleKeyUp('firstname')}
-                            margin="normal"
-                            type="text"
-                        />
-                        <TextField
-                            id="lastname"
-                            label="Lastname"
-                            onKeyUp={this.handleKeyUp('lastname')}
-                            margin="normal"
-                            type="text"
-                        />
-                        <TextField
-                            id="password"
-                            label="Password"
-                            onKeyUp={this.handleKeyUp('password')}
-                            margin="normal"
-                            type="password"
-                        />
-                        <TextField
-                            id="passwordCheck"
-                            label="Password check"
-                            onKeyUp={this.handleKeyUp('passwordCheck')}
-                            margin="normal"
-                            type="password"
-                        />
-                        <Button type="submit" variant="contained" color="primary">
-                            S'enregistrer
-                        </Button>
+                        <FormControl error>
+                            <TextField
+                                id="email"
+                                label="Email"
+                                onKeyUp={this.handleKeyUp('email')}
+                                margin="normal"
+                                type="email"
+                                style={{width: 200}}
+                            />
+                        </FormControl>
+                        <FormControl>
+                            <TextField
+                                id="firstname"
+                                label="Firstname"
+                                onKeyUp={this.handleKeyUp('firstname')}
+                                margin="normal"
+                                type="text"
+                            />
+                        </FormControl>
+                        <FormControl>
+                            <TextField
+                                id="lastname"
+                                label="Lastname"
+                                onKeyUp={this.handleKeyUp('lastname')}
+                                margin="normal"
+                                type="text"
+                            />
+                        </FormControl>
+                        <FormControl>
+                            <TextField
+                                id="password"
+                                label="Password"
+                                onKeyUp={this.handleKeyUp('password')}
+                                margin="normal"
+                                type="password"
+                            />
+                        </FormControl>
+                        <FormControl>
+                            <TextField
+                                id="passwordCheck"
+                                label="Password check"
+                                onKeyUp={this.handleKeyUp('passwordCheck')}
+                                margin="normal"
+                                type="password"
+                            />
+                        </FormControl>
+                        <FormControl>
+                            <Button type="submit" variant="contained" color="primary">
+                                S'enregistrer
+                            </Button>
+                        </FormControl>
                     </FormGroup>
                 </form>
             </Grid>
--- server/src/routes/register.js
+++ server/src/routes/register.js
@@ -1,14 +1,55 @@
 const express = require('express');
 const User = require("../models/user").User;
-
+const bcrypt = require('bcryptjs');
+const { check, validationResult, body } = require('express-validator/check');
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', [
+    check('email').not().isEmpty().isString().isEmail(),
+    check('firstname').not().isEmpty().isString(),
+    check('lastname').not().isEmpty().isString(),
+    check('password').not().isEmpty().isString(),
+    check('passwordCheck').not().isEmpty().isString(),
+    body('passwordCheck').custom((value, { req }) => {
+        if (value === req.body.password) {
+            return true;
+        }
+        throw new Error('Password confirmation does not match password');
+    })
+], (req, res) => {
+    const errors = validationResult(req);
+    let errorsTemplate = {
+        error: "Registration invalid",
+        details: []
+    };
+
+    console.log(errors.array());
+
+    if (!errors.isEmpty()) {
+        const errorsArray = errors.array();
+        for(let i in errorsArray) {
+            errorsTemplate.details.push(errorsArray[i]);
+        }
+        return res.status(400).json(errorsTemplate);
+    }
+
+    // if (req.body.password !== req.body.passwordCheck) {
+    //     const detail = {
+    //         location: 'body',
+    //         param: 'password',
+    //         value: req.body.password,
+    //         msg: 'Password confirmation does not match password'
+    //     };
+    //     errorsTemplate.details.push(detail);
+    //     return res.status(400).json(errorsTemplate);
+    // }
+
     const user = new User({
         email: req.body.email,
         firstname: req.body.firstname,
         lastname: req.body.lastname,
-        password: req.body.password
+        password: req.body.password,
+        passwordCheck: req.body.passwordCheck
     });
 
     user.save().then(data => {
