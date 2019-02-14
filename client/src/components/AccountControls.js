aa6c5f2fb9ad455b79d8590a0ae604fa7ae19fae
Laurent Bassin <laurent@bassin.info>
2019-02-15 00:19:20 +0100

    Badge on cart icon ˚▱˚

--- client/src/components/AccountControls.js
+++ client/src/components/AccountControls.js
@@ -3,10 +3,15 @@ import Button from "@material-ui/core/Button/Button";
 import { ShoppingCart } from '@material-ui/icons';
 import Link from "@material-ui/core/Link/Link";
 import { Link as RouterLink } from 'react-router-dom';
+import Badge from "@material-ui/core/Badge/Badge";
 
 export default ({ user }) => <React.Fragment>
     <Button color="inherit">
-        <Link component={RouterLink} to='/cart' color='inherit'><ShoppingCart/></Link>
+        <Link component={RouterLink} to='/cart' color='inherit'>
+            <Badge badgeContent={user.cart.length} color="secondary">
+                <ShoppingCart/>
+            </Badge>
+        </Link>
     </Button>
     <Link component={RouterLink} to='/security/dashboard'
           color='inherit'
